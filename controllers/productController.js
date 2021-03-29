ad4fad14858aab5c5f72a21068a473d70318572f
jefflin <linjef0711@gmail.com>
2021-03-29 16:52:30 +0800

    refactor : refactor getProducts by using promise all to improve efficacy

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,14 +6,18 @@ const PAGE_OFFSET = 0;
 let productController = {
   getProducts: async (req, res) => {
     try {
-      const products = await Product.findAndCountAll({ raw: true, nest: true, offset: PAGE_OFFSET, limit: PAGE_LIMIT })
-
       let totalPrice = 0
-      const cart = await Cart.findByPk(req.session.cartId, {
-        include: [{ model: Product, as: 'items' }],
-      })
+      let [products, cart] = await Promise.all([
+        Product.findAndCountAll({ raw: true, nest: true, offset: PAGE_OFFSET, limit: PAGE_LIMIT }),
+        Cart.findByPk(req.session.cartId, {
+          include: [{ model: Product, as: 'items' }],
+        })
+      ])
+
       if (!cart) return res.render('products', { products, totalPrice })
+
       totalPrice = cart.items.length > 0 ? cart.items.map((d) => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+
       return res.render('products', { products, totalPrice, cart: cart.toJSON() })
     } catch (error) {
       console.log(error)
