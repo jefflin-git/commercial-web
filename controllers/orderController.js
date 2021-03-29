cca6337d61f494af80db85bf9d094fb80e4fbdc9
jefflin <linjef0711@gmail.com>
2021-03-30 07:54:35 +0800

    feat : add email notification feature when order create

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,14 @@
+const nodemailer = require('nodemailer')
 const db = require('../models')
 const { Order, Product, OrderItem, Cart } = db
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MY_EMAIL,
+    pass: process.env.GMAIL_PASSWORD
+  }
+})
 
 let orderController = {
   getOrders: async (req, res) => {
@@ -18,6 +27,12 @@ let orderController = {
   postOrder: async (req, res) => {
     try {
       const { cartId, name, address, phone, amount, shipping_status, payment_status } = req.body
+
+      if (!name && !phone && !address) {
+        req.flash('error_messages', '請填寫所有表格!')
+        res.redirect('back')
+      }
+
       const [cart, order] = await Promise.all([
         Cart.findByPk(cartId, { include: [{ model: Product, as: 'items' }] }),
         Order.create({
@@ -29,6 +44,12 @@ let orderController = {
           amount
         })
       ])
+
+      if (!cart) {
+        req.flash('error_messages', '購物車內沒有商品，請加入商品!')
+        res.redirect('back')
+      }
+
       const createOrderItem = cart.items.map(product => {
         OrderItem.create({
           OrderId: order.id,
@@ -37,7 +58,23 @@ let orderController = {
           quantity: product.CartItem.quantity
         })
       })
-      await Promise.all([...createOrderItem])
+
+      const mailOptions = {
+        from: process.env.MY_EMAIL,
+        to: address,
+        subject: `Order id: ${order.id} is created`,
+        text: `Order id: ${order.id} is created. You can now go to website and proceed with payment.`
+      }
+      const mailSent = transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log(`Email sent: ${mailSent.response}` + info.response)
+        }
+      })
+
+      await Promise.all([...createOrderItem, mailSent])
+      req.flash('success_messages', '訂單已成立，請查看您的電子信箱!')
       return res.redirect('/orders')
     } catch (error) {
       console.log(error)
