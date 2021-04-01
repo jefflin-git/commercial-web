ee900032dfa391419080ade5f1a966e37bfc634e
jefflin <linjef0711@gmail.com>
2021-04-01 21:43:44 +0800

    feat : add pagination at products page

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,24 +1,34 @@
 const db = require('../models')
 const { Product, Cart } = db
-const PAGE_LIMIT = 3;
-const PAGE_OFFSET = 0;
 
 let productController = {
   getProducts: async (req, res) => {
     try {
+      const pageLimit = 9
+      let pageOffset = 0
+      const page = Number(req.query.page || 1)
+      if (page) {
+        pageOffset = (page - 1) * pageLimit
+      }
+
       let totalPrice = 0
       let [products, cart] = await Promise.all([
-        Product.findAndCountAll({ raw: true, nest: true, offset: PAGE_OFFSET, limit: PAGE_LIMIT }),
+        Product.findAndCountAll({ raw: true, nest: true, offset: pageOffset, limit: pageLimit }),
         Cart.findByPk(req.session.cartId, {
           include: [{ model: Product, as: 'items' }],
         })
       ])
 
-      if (!cart) return res.render('products', { products, totalPrice })
+      const pages = Math.ceil(products.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => item = index + 1)
+      const prev = page - 1 < 1 ? 1 : page - 1
+      const next = page + 1 > pages ? pages : page + 1
+
+      if (!cart) return res.render('products', { products, totalPrice, totalPage, prev, next })
 
       totalPrice = cart.items.length > 0 ? cart.items.map((d) => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
 
-      return res.render('products', { products, totalPrice, cart: cart.toJSON() })
+      return res.render('products', { products, totalPrice, cart: cart.toJSON(), totalPrice, totalPage, prev, next })
     } catch (error) {
       console.log(error)
       res.render('error', { message: 'error !' })
