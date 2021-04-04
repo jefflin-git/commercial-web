9bb539089f9f5f64cfbf0c9bfec506ac6b764f1f
jefflin <linjef0711@gmail.com>
2021-04-04 21:13:14 +0800

    fix : set env setting

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const FacebookStrategy = require('passport-facebook').Strategy
