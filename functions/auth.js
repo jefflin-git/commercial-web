5f6d2078110356470acb0ff60c869906b01c66f0
jefflin <linjef0711@gmail.com>
2021-04-04 20:55:54 +0800

    feat : add facebook login feature

--- functions/auth.js
+++ functions/auth.js
@@ -6,8 +6,8 @@ module.exports = {
       if (helpers.getUser(req).role !== 'admin') {
         return next()
       }
-      req.flash('error_messages', '管理員請使用後台')
-      return res.redirect('/admin/signin')
+      req.flash('error_messages', '權限不足')
+      return res.redirect('/admin/products')
     }
     req.flash('error_messages', '請登入！')
     res.redirect('/signin')
@@ -17,10 +17,10 @@ module.exports = {
       if (helpers.getUser(req).role === 'admin') {
         return next()
       }
-      req.flash('error_messages', '請登入正確帳號!')
-      return res.redirect('/admin/signin')
+      req.flash('error_messages', '權限不足')
+      return res.redirect('/products')
     }
     req.flash('error_messages', '請登入！')
-    res.redirect('/admin/signin')
+    res.redirect('/signin')
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const admin = require('./admin.js')
 const routes = require('./routes.js')
+const auth = require('./auth')
 
 module.exports = app => {
   app.use('/admin', admin)
+  app.use('/auth', auth)
   app.use('/', routes)
 }
