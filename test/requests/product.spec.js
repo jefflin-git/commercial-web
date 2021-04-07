f4ed3ab704406a05ee0131af218b7b616aaab853
jefflin <linjef0711@gmail.com>
2021-04-07 21:41:05 +0800

    test : add test condition into product.spec.js file

--- test/requests/product.spec.js
+++ test/requests/product.spec.js
@@ -8,19 +8,61 @@ const expect = chai.expect
 const db = require('../../models')
 
 describe('# product request', () => {
-  describe('user not login', () => {
-    before((done) => {
-      done()
+  describe('if user want to see all products', () => {
+    before(async () => {
+      await db.Product.create({
+        name: 'name',
+        description: 'description',
+        price: '100',
+        image: `https://loremflickr.com/320/240/furniture/?lock=${Math.random() * 100
+          }`,
+        viewCounts: 0
+      })
     })
-    it('can render product page', (done) => {
+
+    it('can render products page', (done) => {
       request(app)
         .get('/products')
         .set('Accept', 'application/json')
         .expect(200)
         .end(function (err, res) {
           if (err) return done(err)
+          res.text.should.include('name')
           return done()
         })
+    })
+
+    after(async () => {
+      await db.Product.destroy({ where: {}, truncate: true })
+    })
+  })
+
+  describe('if user want to see specific product', () => {
+    before(async () => {
+      await db.Product.create({
+        name: 'name',
+        description: 'description',
+        price: '100',
+        image: `https://loremflickr.com/320/240/furniture/?lock=${Math.random() * 100
+          }`,
+        viewCounts: 0
+      })
+    })
+
+    it('can render products page', (done) => {
+      request(app)
+        .get('/products?id=1')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end(function (err, res) {
+          if (err) return done(err)
+          res.text.should.include('description')
+          return done()
+        })
+    })
+
+    after(async () => {
+      await db.Product.destroy({ where: {}, truncate: true })
     })
   })
 })
