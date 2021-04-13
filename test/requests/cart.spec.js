bf747b11bee0a6d10c344fa2b694671883bb290a
jefflin <linjef0711@gmail.com>
2021-04-13 19:53:42 +0800

    teat : add test condition into cart.spec.js

--- test/requests/cart.spec.js
+++ test/requests/cart.spec.js
@@ -8,7 +8,7 @@ const expect = chai.expect
 const db = require('../../models')
 
 describe('# cart request', () => {
-  describe('show cart page', () => {
+  describe('(1) show cart page', () => {
     before((done) => {
       done()
     })
@@ -23,7 +23,8 @@ describe('# cart request', () => {
         })
     })
   })
-  describe('add product to cart', () => {
+
+  describe('(2) add product to cart', () => {
     before((done) => {
       done()
     })
@@ -49,6 +50,88 @@ describe('# cart request', () => {
 
     after(async () => {
       await db.Cart.destroy({ where: {}, truncate: true })
+    })
+  })
+
+  describe('(3) user can modify quantity of product in the cart', () => {
+    before(async () => {
+      await db.CartItem.create({
+        ProductId: 1,
+        quantity: 1
+      })
+    })
+
+    it('increase quantity', (done) => {
+      request(app)
+        .post('/cartItem/1/add')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if (err) return done(err)
+          db.CartItem.findByPk(1)
+            .then(cartItem => {
+              expect(cartItem).to.equal(2)
+            })
+          return done()
+        })
+    })
+
+    it('decrease quantity', (done) => {
+      request(app)
+        .post('/cartItem/1/sub')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if (err) return done(err)
+          db.CartItem.findByPk(1)
+            .then(cartItem => {
+              expect(cartItem).to.equal(1)
+            })
+          return done()
+        })
+    })
+
+    it('quantity can`t decrease less than one', (done) => {
+      request(app)
+        .post('/cartItem/1/sub')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if (err) return done(err)
+          db.CartItem.findByPk(1)
+            .then(cartItem => {
+              expect(cartItem).to.equal(1)
+            })
+          return done()
+        })
+    })
+
+    after(async () => {
+      await db.CartItem.destroy({ where: {}, truncate: true })
+    })
+  })
+
+  describe('(4) user can delete product in the cart', () => {
+    before(async () => {
+      await db.CartItem.create({
+        ProductId: 1,
+        quantity: 1
+      })
+    })
+
+    it('cat delete successfully', (done) => {
+      request(app)
+        .delete('/cartItem/1')
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if (err) return done(err)
+          db.CartItem.findByPk(1)
+            .then(cartItem => {
+              expect(cartItem).to.be.null
+            })
+          return done()
+        })
+    })
+
+    after(async () => {
+      await db.CartItem.destroy({ where: {}, truncate: true })
     })
   })
 })
