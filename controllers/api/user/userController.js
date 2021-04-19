69190e6f38b4939b9a2ba3d57484332aef9b7470
jefflin <linjef0711@gmail.com>
2021-04-20 07:54:07 +0800

    refactor : refactor signup api logic by async/await and add status code

--- controllers/api/user/userController.js
+++ controllers/api/user/userController.js
@@ -15,16 +15,16 @@ const userController = {
 
       // 檢查必要資料
       if (!username || !password) {
-        return res.status(400).json({ status: 'error', message: "required fields didn't exist" })
+        return res.status(400).json({ status: 'fail', message: "required fields didn't exist" })
       }
 
       const user = await User.findOne({ where: { email: username } })
 
       if (!user) {
-        return res.status(400).json({ status: 'error', message: 'no such user found' })
+        return res.status(400).json({ status: 'fail', message: 'no such user found' })
       }
       if (!bcrypt.compareSync(password, user.password)) {
-        return res.status(400).json({ status: 'error', message: 'passwords did not match' })
+        return res.status(400).json({ status: 'fail', message: 'passwords did not match' })
       }
       // 簽發 token
       var payload = { id: user.id }
@@ -42,34 +42,31 @@ const userController = {
       res.status(500).json({ status: 'error', message: '伺服器錯誤，登入失敗，請再嘗試' })
     }
   },
-  signUp: (req, res) => {
-    const { name, email, password, passwordCheck } = req.body
-    if (!name || !email || !password || !passwordCheck) {
-      return res.json({ status: 'error', message: '請輸入資料！' })
-    }
-    if (passwordCheck !== password) {
-      return res.json({ status: 'error', message: '兩次密碼輸入不同！' })
-    }
+  signUp: async (req, res) => {
+    try {
+      const { name, email, password, passwordCheck } = req.body
+      if (!name || !email || !password || !passwordCheck) {
+        return res.status(400).json({ status: 'fail', message: '請輸入資料！' })
+      }
+      if (passwordCheck !== password) {
+        return res.status(400).json({ status: 'fail', message: '兩次密碼輸入不同！' })
+      }
 
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (user) {
-          return res.json({ status: 'error', message: '信箱重複！' })
-        }
-        User.create({
-          name,
-          email,
-          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
-          role: 'user'
-        })
-          .then(user => {
-            return res.json({ status: 'success', message: '成功註冊帳號！' })
-          })
-          .catch(error => {
-            console.log(error)
-            res.status(500).json({ status: 'fail', message: 'error !' })
-          })
+      let user = await User.findOne({ where: { email } })
+      if (user) {
+        return res.status(400).json({ status: 'fail', message: '信箱重複！' })
+      }
+      user = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null),
+        role: 'user'
       })
+      return res.status(200).json({ status: 'success', message: '成功註冊帳號！' })
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ status: 'error', message: '伺服器錯誤，註冊失敗，請再嘗試' })
+    }
   }
 }
 
