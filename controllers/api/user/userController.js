4ab8b371193c8c5f919f88bc52e363591adcf052
jefflin <linjef0711@gmail.com>
2021-04-20 07:36:27 +0800

    refactor : refactor  jwt signin logic by async/await and add status code

--- controllers/api/user/userController.js
+++ controllers/api/user/userController.js
@@ -8,35 +8,39 @@ const ExtractJwt = passportJWT.ExtractJwt
 const JwtStrategy = passportJWT.Strategy
 
 const userController = {
-  signIn: (req, res) => {
-    let username = req.body.email
-    let password = req.body.password
+  signIn: async (req, res) => {
+    try {
+      let username = req.body.email
+      let password = req.body.password
 
-    // 檢查必要資料
-    if (!username || !password) {
-      return res.json({ status: 'error', message: "required fields didn't exist" })
-    }
+      // 檢查必要資料
+      if (!username || !password) {
+        return res.status(400).json({ status: 'error', message: "required fields didn't exist" })
+      }
 
-    User.findOne({ where: { email: username } })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({ status: 'error', message: 'no such user found' })
+      const user = await User.findOne({ where: { email: username } })
+
+      if (!user) {
+        return res.status(400).json({ status: 'error', message: 'no such user found' })
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(400).json({ status: 'error', message: 'passwords did not match' })
+      }
+      // 簽發 token
+      var payload = { id: user.id }
+      var token = jwt.sign(payload, process.env.JWT_SECRET)
+      return res.status(200).json({
+        status: 'success',
+        message: 'ok',
+        token,
+        user: {
+          id: user.id, name: user.name, email: user.email, role: user.role
         }
-        if (!bcrypt.compareSync(password, user.password)) {
-          return res.status(401).json({ status: 'error', message: 'passwords did not match' })
-        }
-        // 簽發 token
-        var payload = { id: user.id }
-        var token = jwt.sign(payload, process.env.JWT_SECRET)
-        return res.json({
-          status: 'success',
-          message: 'ok',
-          token,
-          user: {
-            id: user.id, name: user.name, email: user.email, role: user.role
-          }
-        })
       })
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ status: 'error', message: '伺服器錯誤，登入失敗，請再嘗試' })
+    }
   },
   signUp: (req, res) => {
     const { name, email, password, passwordCheck } = req.body
